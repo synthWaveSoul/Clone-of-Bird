10cf67e71b4fceec055f08f0c7e5f46d74dcbc9d
Pawel Pawelski <pawel.pawelski.1990@gmail.com>
2023-10-19 16:03:27 +0100

    Add pipe move script

--- Assets/PipeSpawnScript.cs
+++ Assets/PipeSpawnScript.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PipeMoveScript : MonoBehaviour
+public class PipeSpawnScript : MonoBehaviour
 {
-
-    public float moveSpeed = 5;
+    public GameObject pipe;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +15,6 @@ public class PipeMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        
     }
 }
